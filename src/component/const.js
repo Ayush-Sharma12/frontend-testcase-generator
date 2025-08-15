d5602cfadf6c2511072f88549f37ce594187ca64
Ayush-Sharma12 <goodvibes121210@gmail.com>
2025-08-16 00:25:00 +0530

    backend part

--- src/component/const.js
+++ src/component/const.js
@@ -1,3 +1,3 @@
 //const api_base="http://localhost:5000/"
-export const api_base="https://backend-testcase-generator-1654.vercel.app/"
-export const fron_url="https://frontend-testcase-generator-aat7.vercel.app/"
+export const api_base="https://backend-testcase-generator-1654.vercel.app"
+export const fron_url="https://frontend-testcase-generator-aat7.vercel.app"
